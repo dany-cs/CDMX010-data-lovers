badc70413f17105a541b519491f0bd0040382e86
dany correo <danycorreos96@gmail.com>
2021-01-11 18:14:37 -0600

    primer pantalla

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-import { example } from './data.js';
+function pantalla(){
+    document.getElementById("pantalla2").style.display="block";
+    document.getElementById("pantalla1").style.display="none";
+    document.getElementById("nombre").innerHTML= "Bienvenidx " + nomUsuario.value;
+}
+document.getElementById("ingresar").addEventListener("click",pantalla)
+/*import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+console.log(example, data);*/
