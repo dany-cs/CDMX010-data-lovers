271a0155363fb5f200a77a2ed3f7abe30c1cdfa9
dany correo <danycorreos96@gmail.com>
2021-01-12 20:28:53 -0600

    pantalla 1 y 2 completa, pantalla 3 y 4 interfaz completa

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
+//esta pantalla es la principal que lleva al primer menu//
 function pantalla(){
     document.getElementById("pantalla2").style.display="block";
     document.getElementById("pantalla1").style.display="none";
-    document.getElementById("nombre").innerHTML= "Bienvenidx " + nomUsuario.value;
+    document.getElementById("nombre").innerHTML= nomUsuario.value;
 }
-document.getElementById("ingresar").addEventListener("click",pantalla)
+document.getElementById("ingresar").addEventListener("click",pantalla);
+//pantalla que se mira cuando se da click al boton kanto//
+function pantallaDatosK(){
+    document.getElementById("pantalla3").style.display="block";
+    document.getElementById("pantalla2").style.display="none";
+}
+document.getElementById("botonk").addEventListener("click",pantallaDatosK);
+//pantalla que se mira cuando se da click al boton Johto//
+function pantallaDatosJ() {
+    document.getElementById("pantalla4").style.display="block";
+    document.getElementById("pantalla2").style.display="none";
+}
+document.getElementById("botonj").addEventListener("click",pantallaDatosJ);
 /*import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
