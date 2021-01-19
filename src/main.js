fae8a23155e0698ad7343a96712d00266ce4bea2
dany correo <danycorreos96@gmail.com>
2021-01-19 12:04:46 -0600

    prueba de rama

--- src/main.js
+++ src/main.js
@@ -45,9 +45,9 @@ function toggleMenu(event) {
 menu.addEventListener('click',toggleMenu, false);
 
 
-/*import { example } from './data.js';
+//import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+//import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);*/
+//console.log(example, data);*/
