d69b382b1528d7ca7bf9b54f7de30968e7bfeade
katterine-Med <katerinm13@gmail.com>
2021-01-29 22:14:32 -0600

    prueba merge 2

--- src/data.js
+++ src/data.js
@@ -19,19 +19,27 @@ export function datapokemonj() {
 // [1, 2, 3, 4, 5].includes(5) // true
 // [1, 2, 3, 4, 5].includes(0) // false
 // ["fire", "ice", "flying", "psychic"].includes('fire') // true
-/** 
+
 export function filtrarPorDebilidad(debilidad) {
   const pokemonsFiltrados = pokemons.filter(function (pokemon) {
     return pokemon.weaknesses.includes(debilidad);
   })
 
   return pokemonsFiltrados;
-}*/
+}
+
+export function filtrarPorDebilidad(debilidad) {
+  const pokemonsFiltrados = pokemons.filter(function (pokemon) {
+    return pokemon.weaknesses.includes(debilidad);
+  })
+
+  return pokemonsFiltrados;
+}
 
 /**
  * 'pikachu'[0] === 'p' // true
  * 'volbasor'.includes('vol') // true
- 
+ */
 export function filtrarAZ(character) {
   const pokemonsFiltrados = pokemons.filter(function(pokemon){
     return pokemon.name[0] === character
@@ -40,13 +48,5 @@ export function filtrarAZ(character) {
   return pokemonsFiltrados;
 }
 
-<<<<<<< HEAD
-const letra = document.getElementById('letra').value
-filtrarAZ(letra)
-filtrarAZ('a')
-filtrarAZ('b')
-filtrarAZ('c')*/
-=======
 // const letra = document.getElementById('letra').value
 // filtrarAZ(letra)
->>>>>>> katth
